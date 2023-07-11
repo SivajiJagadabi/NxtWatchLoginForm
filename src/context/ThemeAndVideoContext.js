01e800dea635039d79fbffdb6073c734865cac6a
siva <trailnew112@gmail.com>
2023-07-11 11:01:22 +0000

    nxt watch

---
+++ src/context/ThemeAndVideoContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ThemeAndVideoContext = React.createContext({
+  isDarkTheme: false,
+  toggleTheme: () => {},
+  savedVideos: [],
+  addVideo: () => {},
+  activeTab: 'Home',
+  changeTab: () => {},
+})
+
+export default ThemeAndVideoContext
